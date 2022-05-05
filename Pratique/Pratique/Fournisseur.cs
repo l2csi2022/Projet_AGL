894c1775aaf596e1fa2528117f23e9c908af5c84
Henock <hlungelehenock@gmail.com>
2022-05-05 20:12:53 +0200

    Ajout classe Fournisseur par Josué SALEH

---
+++ Pratique/Pratique/Fournisseur.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratique
+{
+    class Fournisseur
+    {
+        private string IdFour;
+
+        public string IdFour1
+        {
+            get { return IdFour; }
+            set { IdFour = value; }
+        }
+        private string NomsFour;
+
+        public string NomsFour1
+        {
+            get { return NomsFour; }
+            set { NomsFour = value; }
+        }
+        private string ContactFour;
+
+        public string ContactFour1
+        {
+            get { return ContactFour; }
+            set { ContactFour = value; }
+        }
+    }
+}
