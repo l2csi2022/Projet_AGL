8795f577a14624b81fb03569c33013574462b6fb
ISAAC <isaactuyish@gmail.com>
2022-05-05 17:29:37 +0200

    ajout de la classe paiment par TUYISHIME HAVUGIMANA ISAAC

---
+++ Pratique/Pratique/paiement.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratique
+{
+    class paiement
+    {
+        private string Idpaiement;
+
+        public string Idpaiement1
+        {
+            get { return Idpaiement; }
+            set { Idpaiement = value; }
+        }
+        private float montat;
+
+        public float Montat
+        {
+            get { return montat; }
+            set { montat = value; }
+        }
+        private string motif;
+
+        public string Motif
+        {
+            get { return motif; }
+            set { motif = value; }
+        }
+        private DateTime datepaiement;
+
+        public DateTime Datepaiement
+        {
+            get { return datepaiement; }
+            set { datepaiement = value; }
+        }
+
+        // LES CONSTRUCTEURS
+
+        // initialisation
+
+        public paiement()
+        { 
+
+        }
+
+        // ajout et modification
+        public paiement(string ID, float MO, string MOT, DateTime DAT)
+        {
+            this.Idpaiement = ID;
+            this.montat = MO;
+            this.motif = MOT;
+            this.datepaiement = DAT;
+        }
+        // suppession
+        public paiement(string ID)
+        {
+            this.Idpaiement = ID;
+        }
+    }
+}
