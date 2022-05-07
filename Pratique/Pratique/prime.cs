23310fb3307bec33e5126f67aa0b2034b8226159
ISAAC <isaactuyish1@gmail.com>
2022-05-07 17:59:23 +0200

    ajout classe prime par KAVUGHO ISEMUKALI EUNICE

---
+++ Pratique/Pratique/prime.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratique
+{
+    class prime
+    {
+        private int idprime;
+
+        public int Idprime
+        {
+            get { return idprime; }
+            set { idprime = value; }
+        }
+        private float montant;
+
+        public float Montant
+        {
+            get { return montant; }
+            set { montant = value; }
+        }
+        private string motifprime;
+
+        public string Motifprime
+        {
+            get { return motifprime; }
+            set { motifprime = value; }
+        }
+        private DateTime datepaieme;
+
+        public DateTime Datepaieme
+        {
+            get { return datepaieme; }
+            set { datepaieme = value; }
+        }
+    }
+}
