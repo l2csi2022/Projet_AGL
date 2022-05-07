cebebcd64e6ec4f685a4b4208b8d5a442e7da0d6
ISAAC <isaactuyish@gmail.com>
2022-05-07 17:12:12 +0200

    ajout de la classe cellule par MUKAMUCHYO BANYANGE ALICE

---
+++ Pratique/Pratique/cellule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratique
+{
+    class cellule
+    {
+        private int numcellule;
+
+        public int Numcellule
+        {
+            get { return numcellule; }
+            set { numcellule = value; }
+        }
+        private string designationcellule;
+
+        public string Designationcellule
+        {
+            get { return designationcellule; }
+            set { designationcellule = value; }
+        }
+        
+    }
+}
