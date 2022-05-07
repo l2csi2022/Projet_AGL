45d2ba3bc09b23b74d7a2a8d53a3d3fc7f75547f
ISAAC <isaactuyish@gmail.com>
2022-05-07 17:29:02 +0200

    ajout de la classe Malalade par KAVIRA VIRGINIE VIVIANE

---
+++ Pratique/Pratique/malade.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratique
+{
+    class malade
+    {
+        private string idmalade;
+
+        public string Idmalade
+        {
+            get { return idmalade; }
+            set { idmalade = value; }
+        }
+        private string nom;
+
+        public string Nom
+        {
+            get { return nom; }
+            set { nom = value; }
+        }
+        private string prenom;
+
+        public string Prenom
+        {
+            get { return prenom; }
+            set { prenom = value; }
+        }
+        private string adresse;
+
+        public string Adresse
+        {
+            get { return adresse; }
+            set { adresse = value; }
+        }
+    }
+}
