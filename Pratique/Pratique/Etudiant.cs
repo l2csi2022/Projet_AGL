596c97a79559daf740a9fbcc95369135797c9e92
l2csi2022 <iscgomal2csi@gmail.com>
2022-05-07 13:23:18 +0200

    Ass KABANZA Justin pour vérifier si le projet n'est pas corrompu

---
+++ Pratique/Pratique/Etudiant.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratique
+{
+    class Etudiant
+    {
+        private string numMat;
+
+        public string NumMat
+        {
+            get { return numMat; }
+            set { numMat = value; }
+        }
+        private string nom;
+
+        public string Nom
+        {
+            get { return nom; }
+            set { nom = value; }
+        }
+        private string postnom;
+
+        public string Postnom
+        {
+            get { return postnom; }
+            set { postnom = value; }
+        }
+        private string sexe;
+
+        public string Sexe
+        {
+            get { return sexe; }
+            set { sexe = value; }
+        }
+
+        public Etudiant()
+        {
+
+        }
+    }
+}
