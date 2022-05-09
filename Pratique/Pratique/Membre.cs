b08e1b7af6a58ef2d0debb13c945b09f0e8032ac
christelle <christellebambaolivia@gmail.com>
2022-05-09 22:16:34 +0200

    MALINGITI Gloire

---
+++ Pratique/Pratique/Membre.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratique
+{
+    //Classe ajoutee par MALINGITI Gloire
+    class Membre
+    {
+    }
+}
