d2a5a792891ae5d6e84c51f54000b7e8cb5d2ee9
l2csi2022 <iscgoma@gmail.com>
2022-05-09 16:03:06 +0200

    TP MULEY LUBWENDE GLODY

---
+++ Pratique/Pratique/Vente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratique
+{
+    class Vente
+    {
+        private string article;
+
+        public string Article
+        {
+            get { return article; }
+            set { article = value; }
+        }
+    }
+}
