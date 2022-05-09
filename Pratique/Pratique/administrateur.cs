a7d180425efe2cbcaeb5da25d7d9a651ec15ea06
christelle <christellebambaolivia@gmail.com>
2022-05-09 10:55:39 +0200

    Ajout classe administrateur par BASILA AYANE Osee

---
+++ Pratique/Pratique/administrateur.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratique
+{
+    // classe ajoutee par BASILA AYANE Osee
+    class administrateur
+    {
+    }
+}
