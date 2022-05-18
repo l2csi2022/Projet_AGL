550dacf4029b9030089a84cc13b5efb84bce22c8
rams <ramskabusala@gmail.com>
2022-05-18 14:40:06 +0200

    Ajout de la classe par RAMAZANI KABUSALA Rams

---
+++ Pratique/Pratique/Recrutement.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratique
+{
+    class Recrutement
+    {
+        private int id_recrutement;
+
+        public int Id_recrutement
+        {
+            get { return id_recrutement; }
+            set { id_recrutement = value; }
+        }
+        private string designation;
+
+        public string Designation
+        {
+            get { return designation; }
+            set { designation = value; }
+        }
+    }
+}
